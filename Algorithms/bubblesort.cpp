21016138ddc6fb95930cb210a6595b7ce94662e7
NereusP <fangyuping0521@gmail.com>
2018-05-05 21:53:28 +0800

    Update bubblesort.cpp

--- Algorithms/bubblesort.cpp
+++ Algorithms/bubblesort.cpp
@@ -1,28 +1,26 @@
-#include <iostream>
+#include <stdio.h>
+#include <utility>
 
 void bubblesort(int a[],unsigned int len);
+void printf_array(int aa[],unsigned int len);
 
 int main(void)
 {
 	int AA[]={2,3,4,5,2,7,5,8,4,8,45,92,89,2,9};
-	for(unsigned int i=0;i<sizeof(AA)/sizeof(int);i++)
-	{
-		std::cout<<AA[i]<<" ";
-	}
-	std::cout<<"\n";
+	printf("before sort:\n");
+	printf_array(AA,sizeof(AA)/sizeof(int));
 	bubblesort(AA,sizeof(AA)/sizeof(int));
-	for(unsigned int i=0;i<sizeof(AA)/sizeof(int);i++)
-	{
-		std::cout<<AA[i]<<" ";
-	}
-	std::cout<<"\n";
+	printf("after sort:\n");
+	printf_array(AA,sizeof(AA)/sizeof(int));
 	return 0;
 }
 
 void bubblesort(int a[],unsigned int len)
 {
+	bool bswap=false;
 	for(unsigned int i=0;i<len-1;i++)
 	{
+		bswap=false;
 		for(unsigned int j=0;j<len-1-i;j++)
 		{
 			if(a[j+1]<a[j])
@@ -30,8 +28,23 @@ void bubblesort(int a[],unsigned int len)
 				int temp=std::move(a[j+1]);
 				a[j+1]=std::move(a[j]);
 				a[j]=std::move(temp);
+				bswap=true;
 			}
+			printf("turn %d-%d:\n",i,j);
+			printf_array(a,len);
+		}
+		if(!bswap)
+		{
+			//If no exchange occurs in one round, the sorting is completed
+			break;
 		}
 	}
 }
-
+void printf_array(int aa[],unsigned int len)
+{
+	for(unsigned int i=0;i<len;i++)
+	{
+		printf("%d ",aa[i]);
+	}
+	printf("\n");
+}
