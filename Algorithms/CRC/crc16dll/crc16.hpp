dd6b88d0f8049ef54dfef3baedfae3063bb3b3ed
NereusP <fangyuping0521@gmail.com>
2018-05-15 09:39:17 +0800

    增加对非文件数据的校验功能

--- Algorithms/CRC/crc16dll/crc16.hpp
+++ Algorithms/CRC/crc16dll/crc16.hpp
@@ -18,17 +18,20 @@ CRC16_MODBUS		0x8005		0xFFFF			Little-endian			XOR with 0x0000
 CRC16_IBM			0x8005		0x0000			Little-endian			XOR with 0x0000
 CRC16_MAXIM			0x8005		0x0000			Little-endian			XOR with 0xFFFF
 CRC16_USB			0x8005		0xFFFF			Little-endian			XOR with 0xFFFF
-
 */
 
 enum CRC16_TYPE { CRC16_CCITT = 0, CRC16_CCITT_FALSE = 1, CRC16_XMODEM = 2, CRC16_X25 = 3, CRC16_MODBUS = 4, CRC16_IBM = 5, CRC16_MAXIM = 6, CRC16_USB = 7 };
 
-#ifdef CRC16DLL_EXPORTS
-extern "C" _declspec(dllexport) int CalcCRC16(FILE* fr, CRC16_TYPE crc16_type, uint16_t& crc16value);
-extern "C" _declspec(dllexport) int CalcCRC16Ex(FILE* fr, uint16_t& crc16value, uint16_t crc_startvalue, uint16_t polyrepres);
+#ifdef CRC16_EXPORTS
+extern "C" _declspec(dllexport) int CalcCRC16(void* buf, uint32_t len, CRC16_TYPE crc16_type, uint16_t& crc16value);
+extern "C" _declspec(dllexport) int CalcCRC16Ex(void* buf, uint32_t len, uint16_t& crc16value, uint16_t crc_startvalue, uint16_t polyrepres);
+extern "C" _declspec(dllexport) int CalcCRC16_file(FILE* fr, CRC16_TYPE crc16_type, uint16_t& crc16value);
+extern "C" _declspec(dllexport) int CalcCRC16Ex_file(FILE* fr, uint16_t& crc16value, uint16_t crc_startvalue, uint16_t polyrepres);
 #else
-extern "C" _declspec(dllimport) int CalcCRC16(FILE* fr, CRC16_TYPE crc16_type, uint16_t& crc16value);
-extern "C" _declspec(dllimport) int CalcCRC16Ex(FILE* fr, uint16_t& crc16value, uint16_t crc_startvalue, uint16_t polyrepres);
+extern "C" _declspec(dllimport) int CalcCRC16(void* buf, uint32_t len, CRC16_TYPE crc16_type, uint16_t& crc16value);
+extern "C" _declspec(dllimport) int CalcCRC16Ex(void* buf, uint32_t len, uint16_t& crc16value, uint16_t crc_startvalue, uint16_t polyrepres);
+extern "C" _declspec(dllimport) int CalcCRC16_file(FILE* fr, CRC16_TYPE crc16_type, uint16_t& crc16value);
+extern "C" _declspec(dllimport) int CalcCRC16Ex_file(FILE* fr, uint16_t& crc16value, uint16_t crc_startvalue, uint16_t polyrepres);
 #endif
 
 #endif
